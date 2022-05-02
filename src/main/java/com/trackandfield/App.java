be76050cff0452eead2245ab6d98475181660842
ITerm0rex <ITerm0rex@gmail.com>
2022-05-02 15:46:14 +0200

    Created method for generating groups.
    Co-authored-by: gorgis1997 <gorgis1997@users.noreply.github.com>
    Co-authored-by: xsvedisx <xsvedisx@users.noreply.github.com>
    Co-authored-by: samiG99 <samiG99@users.noreply.github.com>

--- src/main/java/com/trackandfield/App.java
+++ src/main/java/com/trackandfield/App.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 
 import java.util.concurrent.TimeUnit;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 
 public class App {
@@ -40,7 +41,7 @@ public class App {
 				read.useDelimiter("\n");
 
 				List<Athletes> athletes = new ArrayList<Athletes>();
-				
+
 				for (int j = 1; read.hasNext(); j++) {
 
 					String[] values = read.next().split(",", -1);
@@ -76,21 +77,55 @@ public class App {
 		}
 
 		public List<Groups> generateGroups() {
-
-			var groups = new ArrayList<Groups>();
-
+			int id = 0;
+			List<Groups> groups = new ArrayList<Groups>();
 			List<Athletes> athletes = generateAthletes();
-			
+
+			// var temp = generateAthletes();
+			// List<Athletes> athletes = new ArrayList<Athletes>();
+
+			// for(int i = 0; i < 20; i++)
+			// athletes.add(temp.get(i));
+
+			int count = 0;
+
 			for (var athlete : athletes) {
-				// alt1.sex = "M";
-				// athlete.sorted().sex("M");
-				// athlete.sorted().sex("F");
-				// this.Sex = sex;
-				// System.out.println(athlete);
+
+				AGE_GROUPS athlete_ageGroup = Groups.getAgeGroup(athlete);
+				SEX_GROUPS athlete_sexGroup = Groups.getsexGroup(athlete);
+				List<DISCIPLINES> athlete_disciplines = Groups.getDisciplines(athlete);
+
+				for (var athlete_discipline : athlete_disciplines) {
+					boolean not_found = true;
+
+					for (var group : groups) {
+						if (group.age == athlete_ageGroup && group.sex == athlete_sexGroup
+								&& group.discipline == athlete_discipline) {
+							if (!group.athletes.contains(athlete)) {
+								group.athletes.add(athlete);
+								not_found = false;
+							}
+						}
+						count++;
+					}
+					if (not_found) {
+						var new_group = new Groups(
+								++id,
+								athlete_ageGroup,
+								athlete_sexGroup,
+								athlete_discipline,
+								new ArrayList<Athletes>());
+						groups.add(new_group);
+						new_group.athletes.add(athlete);
+					}
+
+				}
 
 			}
 
-			return null;
+			System.err.println(count);
+
+			return groups;
 		}
 
 	}
@@ -106,40 +141,106 @@ public class App {
 		INVALID
 	}
 
+	enum SEX_GROUPS {
+		M,
+		F,
+		INVALID
+	}
+
+	enum DISCIPLINES {
+		Running_Sprint_60,
+		Running_Sprint_200,
+		Running_Middle_800,
+		Running_Middle_1500,
+		Running_Long_3000,
+		Running_Hurdles_60,
+		Jumping_Long,
+		Jumping_Triple,
+		Jumping_High,
+		Jumping_Pole,
+		Throwing_Shot,
+		INVALID
+	}
+
 	class Groups {
-		// final static List<int[]> Age_Intervalsss = ;
-
-		final static int[][] Age_Intervals = { { 0, 8 }, { 9, 10 }, { 11, 12 }, { 13, 14 }, { 15, 16 }, { 17, 18 },
-				{ 19, 0x7FFFFFFF /* biggest positive integer */ } };
-		final static char[] Sexes = { 'M', 'F' };
-
 		int id;
-		int age;
-		char sex;
-		String name;
-		List<Athletes> Athletes;
+		AGE_GROUPS age;
+		SEX_GROUPS sex;
+		DISCIPLINES discipline;
+		List<Athletes> athletes;
 
-		public AGE_GROUPS getAgeGroup(Athletes atl) {
+		public Groups(int id, AGE_GROUPS age, SEX_GROUPS sex, DISCIPLINES discipline,
+				List<com.trackandfield.App.Athletes> athletes) {
+			this.id = id;
+			this.age = age;
+			this.sex = sex;
+			this.discipline = discipline;
+			this.athletes = athletes;
+		}
 
-			if (atl.age < 8)
+		public void AddAthlete(Athletes athlete) {
+			athletes.add(athlete);
+		}
+
+		public static List<DISCIPLINES> getDisciplines(Athletes athlete) {
+			// dis is a list of all disiplines
+			List<DISCIPLINES> dis = new ArrayList<DISCIPLINES>(Arrays.asList(DISCIPLINES.values()));
+
+			int i = 0;
+			// Removes the corresponding discipline if record is empty
+			for (var rec : athlete.records) {
+				if (rec.isEmpty())
+					dis.remove(i);
+				else
+					i++;
+			}
+			dis.remove(DISCIPLINES.INVALID);
+			return dis;
+		}
+
+		// Check and return the sex of an athlete
+		public static SEX_GROUPS getsexGroup(Athletes athlete) {
+			if (athlete.sex == 'M')
+				return SEX_GROUPS.M;
+
+			if (athlete.sex == 'F')
+				return SEX_GROUPS.F;
+
+			return SEX_GROUPS.INVALID;
+		}
+
+		// Check and return the age group of an athlete
+		public static AGE_GROUPS getAgeGroup(Athletes athlete) {
+			if (athlete.age <= 8)
 				return AGE_GROUPS._8;
 
-			if (atl.age == 17 || atl.age == 18)
+			if (athlete.age == 9 || athlete.age == 10)
+				return AGE_GROUPS._9_10;
+
+			if (athlete.age == 11 || athlete.age == 12)
+				return AGE_GROUPS._11_12;
+
+			if (athlete.age == 13 || athlete.age == 14)
+				return AGE_GROUPS._13_14;
+
+			if (athlete.age == 15 || athlete.age == 16)
+				return AGE_GROUPS._15_16;
+
+			if (athlete.age == 17 || athlete.age == 18)
 				return AGE_GROUPS._17_18;
 
-			if (atl.age > 18)
+			if (athlete.age > 18)
 				return AGE_GROUPS.ADOULT;
 
 			return AGE_GROUPS.INVALID;
 		}
 
-		public Groups(int id, int age, char sex, String name, List<com.trackandfield.App.Athletes> athletes) {
-			this.id = id;
-			this.age = age;
-			this.sex = sex;
-			this.name = name;
-			Athletes = athletes;
+		@Override
+		public String toString() {
+			return "Groups [age=" + age + ", athletes#=" + athletes.size() + ", discipline=" + discipline + ", id=" + id
+					+ ", sex=" + sex + "]";
 		}
+
 	}
 
 	class Athletes {
@@ -242,9 +343,32 @@ public class App {
 
 		io test = a.new io();
 
-		for (var athlete : test.generateAthletes()) {
-			System.out.println(athlete.id);
+		// var atl = test.generateAthletes();
+
+		// for (var athlete : atl) {
+		// System.out.println(athlete.id);
+		// }
+
+		// System.out.println(App.Groups.getDisciplines(atl.get(0)));
+		// System.out.println(App.Groups.getAgeGroup(atl.get(0)));
+		// System.out.println(App.Groups.getsexGroup(atl.get(0)));
+
+		// a.new Groups(0, atl.athlets , al1.athlete_sexGroup,at1.athlete_discipline
+		// discipline, athletes)
+
+		var gs = test.generateGroups();
+
+		for (var g : gs) {
+			System.out.println(g.toString());
 		}
+
+		// String s = " " + gs.get(0);
+
+		// for (var g : gs) {
+		// System.out.println(g.athletes.size());
+		// }
+
+		// System.out.println(gs.size());
 
 		// System.out.println(", ,,".split(",")[0]);
 
--- src/test/java/com/trackandfield/AppTest.java
+++ src/test/java/com/trackandfield/AppTest.java
@@ -2,6 +2,11 @@ package com.trackandfield;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import com.trackandfield.App.io;
+
 import org.junit.Test;
 
 /**
@@ -14,5 +19,32 @@ public class AppTest {
 	@Test
 	public void shouldAnswerWithTrue() {
 		assertTrue(true);
+	}
+	
+	@Test
+	public void testLength() {
+		// var a = new App();
+		// io test = a.new io();
+		// var atl = test.generateAthletes();
+		var rec = new String[]{"","","","","","","","","","",""};
+		
+		rec[1] = "0:0";
+		
+		var atl = new App().new Athletes(0, "club", "name", "surname", 'M', 18, Arrays.asList(rec));
+
+		
+		var len = App.Groups.getDisciplines(atl).size();
+		
+		assertTrue("length is " + len, len == 1);
+	}
+	
+	@Test
+	public void testFromFile() {
+		var a = new App();
+		io test = a.new io();
+		var atl = test.generateAthletes();
+		
+		var len = App.Groups.getDisciplines(atl.get(1)).size();
+		assertTrue("length is " + len, len == 4);
 	}
 }
