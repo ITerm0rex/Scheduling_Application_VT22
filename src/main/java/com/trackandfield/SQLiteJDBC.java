49eee113ea1fdb699d3ff96593f4c5f38a600e5b
ITerm0rex <ITerm0rex@gmail.com>
2022-05-04 18:08:27 +0200

    Improved prototype for reading CSV using sqlite and csv.dll module.

--- src/main/java/com/trackandfield/SQLiteJDBC.java
+++ src/main/java/com/trackandfield/SQLiteJDBC.java
@@ -1,53 +1,64 @@
 package com.trackandfield;
 
 import java.sql.*;
+import java.util.Properties;
 
 public class SQLiteJDBC {
+	private static final String Athletes_SQL = """
+			create table Athletes (
+				Club varchar(64),
+				Name varchar(64),
+				Surname varchar(64),
+				Sex varchar(1),
+				Age integer(3),
+				Running_Sprint_60 time,
+				Running_Sprint_200 time,
+				Running_Middle_800 time,
+				Running_Middle_1500 time,
+				Running_Long_3000 time,
+				Running_Hurdles_60 time,
+				Jumping_Long float,
+				Jumping_Triple float,
+				Jumping_High float,
+				Jumping_Pole float,
+				Throwing_Shot float
+			);
+			""";
+
 	public static void run() {
-		Connection c = null;
-
 		try {
+			var prop = new Properties();
+			prop.setProperty("enable_load_extension", "true");
 			Class.forName("org.sqlite.JDBC");
-			// c = DriverManager.getConnection("jdbc:sqlite:./resources/test.db");
-			c = DriverManager.getConnection("jdbc:sqlite::memory:");
+			try (var con = DriverManager.getConnection("jdbc:sqlite::memory:", prop);
+					var stmt = con.createStatement()) {
 
-			var stmt = c.createStatement();
+				stmt.execute("select load_extension(\"./sqlite-csv/csv.dll\");");
 
-			{
-				stmt.execute("""
-						create table Athletes (
-							Club varchar(32),
-							Name varchar(32),
-							Surname varchar(32),
-							Sex varchar(1),
-							Age intiger(10),
-							Running_Sprint_60 date,
-							Running_Sprint_200 date,
-							Running_Middle_800 date,
-							Running_Middle_1500 date,
-							Running_Long_3000 date,
-							Running_Hurdles_60 date,
-							Jumping_Long float,
-							Jumping_Triple float,
-							Jumping_High float,
-							Jumping_Pole float,
-							Throwing_Shot float
-						);
-						""");
+				stmt.execute("CREATE VIRTUAL TABLE IF NOT EXISTS temp.csv USING csv("
+						// + "filename=\"./resources/Schema_Exempel.csv\""
+						// + ",header=yes"
+						// + ",fsep=','"
+						// + ",rsep='\n'"
+						+ "filename = \"./resources/registration-list.csv\""
+						+ ",schema=\"" + Athletes_SQL + "\""
+						+ ");");
+
+				try (var rs = stmt.executeQuery("select * from csv;")) {
+					var md = rs.getMetaData();
+					var colName = md.getColumnName(3);
+
+					System.err.println(":#" + md.getColumnCount());
+					System.err.println("::" + colName);
+
+					while (rs.next())
+						System.out.println(rs.getObject(colName));
+				}
 			}
-
-			var rs = stmt.executeQuery("select 1+1;");
-
-			while (rs.next())
-				System.out.println(rs.getInt(1));
-
-			stmt.closeOnCompletion();
-			c.close();
-
 		} catch (Exception e) {
 			System.err.println(e.getClass().getName() + ": " + e.getMessage());
 			System.exit(0);
 		}
-		System.out.println("Opened database successfully");
+		System.out.println("[Opened database successfully]");
 	}
 }
